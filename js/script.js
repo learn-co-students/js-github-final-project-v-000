78fe0f11219a3a7ced598ae128a12b60ffc77c89
Lukeghenco <lukeghenco@gmail.com>
2016-02-12 17:00:56 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,62 @@
-function GithubInteractor() {
+var newInteraction = new GithubInteractor("4795d3e4a3930f9875764bb346eae24d98e7bdd1")
 
+$(document).ready(function() {
+  submitForm();
+});
+
+function GithubInteractor(token) {
+  this.token = token;
+  this.api = "https://api.github.com/repos/";
 }
 
-GithubInteractor.prototype.createIssue = function() {
+
+function Issue(url, title, body) {
+  this.url = url;
+  this.title = title;
+  this.body = body;
+}
+
+Issue.prototype.renderIssue = function(selector){
+  var link = $('<a>')
+    .attr('href', this.issueURL)
+    .text(this.title);
+    selector.append(link);
+}
+
+function createIssue(repoName, repoOwner, title, body) {
+  var url = newInteraction.api + repoOwner + "/" + repoName + "/issues";
+  var data = {
+    "title": title,
+    "body": body
+  }
   $.ajax({
-    url: 'https://
+    type: "POST",
+    url: url,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "token " + newInteraction.token);
+    },
+    data: JSON.stringify(data)
   })
+  .done(handleResponse)
+  .fail(handleError);
+}
+
+function handleResponse(response) {
+  var issue = new Issue(response.url, response.title, response.body)
+  issue.renderIssue($('#issue'));
+}
+
+function handleError(XHR, status, error) {
+  console.log("Post error: " + error);
+}
+
+function submitForm() {
+  $('form').on("submit", function(event) {
+    var repoName = $('#repoName').val();
+    var repoOwner = $('#repoOwner').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+    createIssue(repoName, repoOwner, title, body);
+    event.preventDefault();
+  });
 }
