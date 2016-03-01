747c95a570f6c923dc948e4668c2a078e2823abb
selichter <lichter.sarahe@gmail.com>
2016-02-29 22:17:27 -0600

    Done.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function GithubInteractor(token) {
+  this.token = token;
+}
+
+var interactor = new GithubInteractor("");
+
+function handleError(error) {
+  var message = error.statusText
+  if (message == undefined) {message = 'Unauthorized'};
+  console.log('Post error: ' + message);
+};
+
+function handleResponse(response) {
+  var html = '<a href="'+response.html_url+'">'+response.title+'</a>';
+  $('#issue').html(html);
+};
+
+function createIssue(repoName, repoOwner, title, body) {
+  console.log("creating issue...");
+  var postUrl = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/issues';
+  var postData = {title: title, body: body};
+  $.ajax({
+		url: postUrl,
+		type: 'POST',
+		beforeSend: function(xhr) {
+    		xhr.setRequestHeader("Authorization", "token "+interactor.token);
+    	},
+    	data: JSON.stringify(postData)
+	}).done(handleResponse).fail(handleError);
+}
+
+function bindSubmit(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		var name = $('#repoName').val();
+		var owner = $('#repoOwner').val();
+		var name = $('#title').val();
+		var name = $('#body').val();
+		createIssue(name, owner, title, body);
+	});
+}
+
+$( document ).ready(function() {
+	bindSubmit();
+    console.log( "ready!" );
+});
