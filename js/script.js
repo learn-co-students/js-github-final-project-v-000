4ef620bd38d8111e7b96612967b8a5b98644fe4c
Adam King <adam.king0126@gmail.com>
2016-03-24 16:58:06 -0500

    Done.

---
+++ js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+$(document).ready(function() {
+  $('submit').on('click', function(){
+    var repoName = $('#repoName').val();
+    var repoOwner = $('#repoOwner').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+    createIssue(repoName, repoOwner, title, body);
+  });
+});
+
+function GithubInteractor(token){
+  this.token = token;
+  this.apiBaseURL = "https://api.github.com/repos/";
+}
+
+var interactor = new GithubInteractor("wasdfwasd");
+
+function createIssue(repoName, repoOwner, title, body) {
+  var url = interactor.apiBaseURL + repoOwner + "/" + repoName + "/issues";
+  var data = {title: title, body: body};
+  $.ajax({
+    url: url,
+    type: "POST",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "token " + interactor.token);
+    },
+    data: JSON.stringify(data) })
+    .done(handleResponse)
+    .error(handleError);
+}
+    
+function handleResponse(response){
+  console.log("in handleResponse");
+  $('#issue').append("<p>" + response.title + "</p>");
+}
+
+function handleError(error){
+  if(Object.keys(error).length > 0){
+    console.log("we've got  an error");
+    var e = "Post error: " + error.statusText;
+    console.log(e);
+  }
+}
+
+
+
