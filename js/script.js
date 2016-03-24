3ad0a66675858a74440a527d46ce566e58ab22d0
lisamarie616 <lisamarie616@gmail.com>
2016-03-24 16:55:41 -0700

    Done.

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  submitForm();
+})
+
+function GithubInteractor(token){
+  this.token = token;
+  this.apiBaseUrl = "https://api.github.com/repos/";
+}
+
+var interactor = new GithubInteractor("c0c20f0e2c36bc772e8dc4027221f548064d0dcc");
+
+function createIssue(repoName, repoOwner, title, body){
+  var url = interactor.apiBaseUrl + repoOwner + "/" + repoName + "/issues";
+  var data = {
+      "title": title,
+      "body": body
+     };
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "token " + interactor.token);
+    },
+    data: JSON.stringify(data)
+  })
+  .done(handleResponse)
+  .fail(handleError);
+}
+
+function handleResponse(response){
+  var html = "<a href='" + response.html_url+ "'>" + response.title + "</a>";
+  $('#issue').append(html);
+}
+
+function handleError(jqXHR, textStatus, errorThrown){
+  console.log("Post error: " + errorThrown);
+}
+
+function submitForm(){
+  $('form').on('submit', function(event){
+    var repoName = $('#repoName').val();
+    var repoOwner = $('#repoOwner').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+    createIssue(repoName, repoOwner, title, body);
+    event.preventDefault();
+  });
+}
