0a523ac9e035fd7e54777d13a40e8d212418c5d3
Brett <brettrheenan@gmail.com>
2016-03-29 10:09:48 +0200

    Done.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var token = "7b8eed31d1b6354640410c70eb536b7ac5666ea3"
-var baseUrl = "https://api.github.com/repos/"
+var authToken = "7b8eed31d1b6354640410c70eb536b7ac5666ea3"
 
 
 $(document).ready(function() {
@@ -12,40 +11,51 @@ $(document).ready(function() {
 
 function GithubInteractor(token) {
 	this.token = token
+	this.baseUrl = "https://api.github.com/repos/"
 }
 
+function Issue(issueUrl, title, body) {
+	this.issueUrl = issueUrl
+	this.title = title
+	this.body = body
+}
 
+Issue.prototype.renderIssue = function(selector) {
+	var link = $('<a>')
+	.attr('href', this.issueUrl)
+	.text(this.title);
+	selector.append(link);
+}
 
-function createIssue(repoName, repoOwner, title, body, token) {
+var interactor = new GithubInteractor(authToken)
+
+function createIssue(repoName, repoOwner, title, body) {
+	var data = { title: title, body: body }
 	$.ajax({
-		url: baseUrl + repoOwner + '/' + repoName + '/issues',
+		url: interactor.baseUrl + repoOwner + '/' + repoName + '/issues',
 		type: "POST",
 		dataType: 'json',
-		data: JSON.stringify({
-			title: title,
-			body: body
-		}),
+		data: JSON.stringify(data),
 		beforeSend: function(xhr) {
-			xhr.setRequestHeader("Authorization", "token " + token); 
-		},
-		success: function(response) {
-			handleResponse(response);
+			xhr.setRequestHeader("Authorization", "token " + interactor.token); 
 		}
-	}).fail(function(jqXHR, textStatus, errorThrown) {
-		handleError(jqXHR, textStatus, errorThrown);
-	});
-};
+	})
+	.done(handleResponse)
+	.fail(handleError);
+}
 
 function getIssue() {
 	var repoName = $("#repoName").val();
 	var repoOwner = $("#repoOwner").val();
 	var title = $("#title").val();
 	var body = $("#body").val();
-	createIssue(repoName, repoOwner, title, body, token);
+	createIssue(repoName, repoOwner, title, body);
 };
 
 function handleResponse(response) {
-	$('#issue').append('<div><a href="' + response.html_url + '">' + response.title + '</a></div>');
+	var issue = new Issue(response.html_url, response.title, response.body);
+	issue.renderIssue($('#issue'));
+	// $('#issue').append('<div><a href="' + response.html_url + '">' + response.title + '</a></div>');
 }
 
 function handleError(jqXHR, textStatus, errorThrown) {
