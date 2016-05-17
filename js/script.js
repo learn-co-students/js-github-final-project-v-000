118c15ed76f9b98bbd4ead0f32113c43c589bfb8
mmmeyers <marsi286@gmail.com>
2016-05-17 13:08:16 -0400

    Done.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var token = "cbbdfedda51deb1f87ef74debcd7b45278f15a55"
+
+$(document).ready(function() {
+  submitForm();
+});
+
+function submitForm() {
+  $("form").on("submit", function(e) {
+    var repoName = $("#repoName").val();
+    var repoOwner = $("#repoOwner").val();
+    var title = $("#title").val();
+    var body = $("#body").val();
+    createIssue(repoName, repoOwner, title, body);
+    e.preventDefault();
+  });
+}
+
+function GithubInteractor(token) {
+  this.token = token;
+}
+
+function handleResponse(response) {
+  $("#issue").text(response.title);
+}
+
+function handleError(error) {
+  console.log("Post error: Unauthorized");
+}
+
+function createIssue(repoName, repoOwner, title, body) {
+  var url = "https://api.github.com/repos/"+repoOwner+"/"+repoName+"/issues"
+  var data = {
+    title: title,
+    body: body
+  }
+  $.ajax({
+    url: url,
+    type: 'POST',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "token " + token);
+    },
+    data: JSON.stringify(data)
+  }).done(handleResponse)
+  .fail(handleError);
+}
+
