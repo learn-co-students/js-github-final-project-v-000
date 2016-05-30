dcffe35bdf74fed73e92d946402a95c18a3275a3
Tyler Smith <oats000@hotmail.com>
2016-05-30 10:06:52 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
+function GithubInteractor(token) {
+  this.token = token;
+}
+
+var interactor = new GithubInteractor("078d09aa0b3dbf4bf2e0b53da2c3d735f9d06d73");
 
 var createIssue = function(repoName, repoOwner, title, body){
+  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName +'/issues';
 
   $.ajax({
-      url: '/repos/' + repoOwner + '/' + repoName +'/issues',
-      type: 'POST',
-      dataType: 'json',
-      title: title,
-      body: body
+    method: "POST",
+    url: url,
+    contentType: "application/json",
+    dataType: 'json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "token " + interactor.token);
+    },
+    data: JSON.stringify({title: title,body: body})
   }).done(function(response) {
-      $('#list').append(
-        '<li><a href="'+response.url+'">'+response.title+'</a></li>'
-      );
-  });
+      $('#issue').append('<li><a href="'+response.url+'">'+response.title+'</a></li>');
+    }).error(function(error) {
+      // debugger;
+      console.log("Post error: " + error.title)
+    });
 }
 
 var submitForm = function() {
   // call functions here
-  $('#submit').on('click', function () {
+  $('form').on('submit', function (e) {
     var repoName = $('#repoName').val();
     var repoOwner = $('#repoOwner').val();
-    var title = $('#tile').val();
+    var title = $('#title').val();
     var body = $('#body').val();
 
     createIssue(repoName, repoOwner, title, body);
+    e.preventDefault();
   });
 };
 
