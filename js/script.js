2bd9f188d15e5a1e13f54420fd21f438e9f54d71
Peter Jacobson <github email address>
2016-07-16 15:23:26 -0400

    Done.

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function GithubInteractor(token){
+  this.token = token;
+  this.apiBaseUrl = "https://api.github.com/repos/";
+}
+
+var interactor = new GithubInteractor("329407cab55242a810f514392b972102cbbcb3ad");
+
+function submitForm() {
+  $('#create-issue').click(function (event) {
+    var repoName = $('#repoName').val();
+    var repoOwner = $('#repoOwner').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+    createIssue(repoName, repoOwner, title, body);
+    event.preventDefault();
+  })
+}
+
+
+var createIssue = function(repoName, repoOwner, title, body){
+  var url = interactor.apiBaseUrl + repoOwner + "/" + repoName + "/issues"
+
+  $.ajax({
+    url: url,
+    type: 'POST',
+    dataType: 'json',
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", "token " + interactor.token);
+    },
+    data: JSON.stringify({ title : title, body : body}),
+    success: function (result) {
+      handleResponse(result);
+    },
+    error: function (result) {
+      var errorThrown = result.status;
+      var textStatus = result.statusText;
+      var jqXHR = {}
+      handleError(jqXHR, textStatus, errorThrown)
+    },
+  })
+};
+
+function handleResponse(result) {
+  $('#issue').append(result.title);
+}
+
+function handleError(jqXHR, textStatus, errorThrown) {
+  console.log("Post error: " + errorThrown);
+}
+
+$(document).ready(function(){
+  submitForm();
+});
