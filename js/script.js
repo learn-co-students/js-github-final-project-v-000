fe51a222a3c56577ebef2da62e3172f015dea61f
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-10 20:29:45 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 // function that submits form, and on submit it should call createIssue
-// 'use strict';
 
-// class GithubInteractor{
-//   constructor(token) {
-//     this.token = token;
-//   }
+var interactor = new GithubInteractor("6fbe62e9d1f8c5b93eca40363899eac7533d891a");
 
-  
-
-// }
 $(document).ready(function() {
   submitForm();
 });
 
+// this function works but is not getting called on submit
 
+function createIssue(repo, owner, title, body) {
+  var data = { 
+      title: title,
+      body: body
+  };
+
+  var auth = {
+    Authorization: "token "+interactor.token
+  };
+
+  $.ajax({
+    url: 'https://api.github.com/repos/'+owner+'/'+repo+'/issues',
+    type: 'POST',
+    dataType: 'jsonp',
+    data: JSON.stringify(data),
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization: ", "token " + interactor.token);
+    }
+    // headers: auth  
+    // success: function(response){
+    //   handleResponse(response);
+    // },
+    // error: function(xhr, textStatus, errorThrown){
+    //  handleError(xhr, textStatus, errorThrown);
+    // }
+  }).done(handleResponse).fail(handleError);
+}
+
+function handleResponse(response)
+{
+  $.each(response.data, function(index, item) {
+    $('#response-urls').append("<li><a href="+item.html_url+">"+item.html_url+"</a></li>");
+    $('#response-titles').append("<li>"+item.title+"</li>");
+    $('#response-bodys').append("<li>"+item.body+"</li>");    
+  })
+}
 
 function submitForm() {
   $('form').on("submit", function(event) {
@@ -22,69 +52,20 @@ function submitForm() {
         title     = $("input#title").val(),
         body      = $("input#body").val();
 
-    // var interactor = new GithubInteractor("6fbe62e9d1f8c5b93eca40363899eac7533d891a");
+    // those variables are html elements, fix them
     debugger
-    // interactor.prototype.createIssue(repoName, repoOwner, title, body);
     createIssue(repoName, repoOwner, title, body);
   });
 
 }
 
+function handleError(XHR, textStatus, errorThrown) {
+  // body...
+  debugger
+  console.log("Post error: " + errorThrown)
+}
 
 function GithubInteractor(token) {
   this.token = token;
-
-  handleResponse = function(response) {
-    debugger
-
-    $.each(response, function(index, element) {
-      $('#issue').append(
-          $('<a>').text(element.html_url),
-          $('<h2>').text(element.title),
-          $('<p>').text(element.body)
-        )
-    })
-  }
-  
 }
 
-var createIssue = function(repo, owner, title, body) {
-    debugger
-    $.ajax({
-      url: 'https://api.github.com/repos/'+owner+'/'+repo+'/issues',
-      type: 'POST',
-      data: JSON.stringify({ 
-        title: title,
-        body: body
-      }),
-      success: function(response) {
-        debugger
-        handleResponse(response)
-      }
-      // dataType: 'json',
-    //   // headers: {
-    //   //   Authorization: 'token '+this.token;
-    //   // },
-    });
-  }
-
-// var createIssue = function(repo, owner, title, body) {
-//   // body...
-//   $.ajax({
-//     url: 'https://api.github.com/repos/'+owner+'/'+repo+'/issues'
-//     type: 'POST',
-//     dataType: 'json',
-//     headers: {
-//       Authorization: 'token '+this.token;
-//     },
-    
-//   })
-
-// function GithubInteractor(token) {
-//   this.token = token;
-
-
-
-
-
-// }
