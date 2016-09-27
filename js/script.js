b02cad604492fb87f2f4952725413a8c1f9248aa
arturusfury <bri.reynolds@gmail.com>
2016-09-27 10:34:05 -0500

    Done.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Github Token: 79c2fe5df35240048ab2b9a2ed9023b6607d9b3d
+function GithubInteractor(token){
+	this.token = token;
+}
+
+function submitForm(){
+	$('form').on('submit', function(e){
+		e.preventDefault();
+		var repoName = $('#repoName').val();
+		var repoOwner = $('#repoOwner').val();
+		var issueTitle = $('#title').val();
+		var issueBody = $('#body').val();
+		createIssue(repoName,repoOwner, issueTitle, issueBody);
+	})
+}
+
+function createIssue(repoName, repoOwner, issueTitle, issueBody){
+	console.log(repoName + repoOwner + issueTitle + issueBody);
+	var postData = {
+      'title':   issueTitle,
+      'body': issueBody,
+    };
+    var postURL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/issues';
+    console.log(postData);
+    $.ajax({
+	  type: "POST",
+	  url: postURL,
+	  data: JSON.stringify(postData),
+	})
+		.done(function(data) {
+			console.log(data);
+			handleResponse(data);
+		})
+		.fail(function(data) {
+			console.log(data);
+			handleError(data);
+		})
+}
+
+function handleResponse(data){
+	$('#issue').append(data.title);
+}
+
+function handleError(){
+    console.log("Post error: Unauthorized");
+}
