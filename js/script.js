2f8c37969ef963aea1195a5e5999f3881636b914
Christian Whitesides <whitesidescr@gmail.com>
2016-10-31 17:37:56 -0700

    Done.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ function GithubInteractor(token) {
   this.token = token;
 }
 
+var interactor = new GithubInteractor("SomethingMadeUp")
+
 function handleResponse(response) {
   $('#issue').html(response.title)
 }
 
-function handleError(response) {
-  console.log('Post error: Unauthorized');
+function handleError(jqXHR, textStatus, errorThrown) {
+  // See: http://stackoverflow.com/questions/9847244/what-are-the-parameters-sent-to-fail-in-jquery
+  console.log('Post error: ' + errorThrown);
 }
 
 function createIssue(repositoryName, repositoryOwner, title, body) {
@@ -19,7 +22,9 @@ function createIssue(repositoryName, repositoryOwner, title, body) {
   $.ajax({
     url: 'https://api.github.com/repos/' + repositoryOwner + '/' + repositoryName + '/issues',
     type: 'POST',
-    dataType: 'json',
+    beforeSend: function(xhr) {
+     xhr.setRequestHeader("Authorization", "token " + interactor.token);
+    },
     data: JSON.stringify(data)
   })
   .done(handleResponse)
@@ -27,7 +32,14 @@ function createIssue(repositoryName, repositoryOwner, title, body) {
 }
 
 function submitForm() {
-
+  $('form').on('submit', function(event){
+    var repoName = $('#repoName').val();
+    var repoOwner = $('#repoOwner').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+    createIssue(repoName, repoOwner, title, body);
+    event.preventDefault();
+  });
 }
 
 $(document).ready(function() {
