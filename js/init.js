4b8e1924a0621df8901421a02e917a3a2db36dc4
jbok4 <jbok4@aol.com>
2016-11-02 17:21:12 -0400

    change name of APPI

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "10a218cb7e1e7a2db3805960cb376bb290145f85";
+  var GitKey = "10a218cb7e1e7a2db3805960cb376bb290145f85";
   var URL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/issues';
 
   submitForm();
