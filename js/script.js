c7c59314e39e3c26b159c8fc7c420a8f7987b6fc
tpstar <hanlee12@gmail.com>
2016-11-09 23:57:49 -0500

    post data

--- js/script.js
+++ js/script.js
@@ -2,26 +2,43 @@ function GithubInteractor(token) {
   this.token = token;
 }
 
+var tokenHolder = new GithubInteractor("4b55b2eab6734859d021d2fe62ce251b42721541")
+
 var createIssue = function(repoName, repoOwner, issueTitle, issueBody){
   var data = {
-    'public':   true,
-    'description': description,
-    'files': {}
+    'title': issueTitle,
+    'body': issueBody
   };
-
-  data['files'][file_name] = {
-    'content': content
-  };
-
-  $.ajax({
+  // alert('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/issues')
+  var postData = $.ajax({
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/issues',
     type: 'POST',
     dataType: 'json',
     beforeSend: function(xhr) {
-      xhr.setRequestHeader("Authorization", "token " + token);
+      xhr.setRequestHeader("Authorization", "token " + tokenHolder);
     },
     data: JSON.stringify(data)
-  }).done(function(response) {
-    // some action
+    // headers: {
+    //   Authorization: "4b55b2eab6734859d021d2fe62ce251b42721541"
+    // }
+  });
+  // postData.done(handleResponse(response))
+};
+
+
+
+var bindSubmitButton = function() {
+  // call functions here
+  $('#submit').click(function() {
+    var repoName = $('#repoName').val();
+    var repoOwner = $('#repoOwner').val();
+    var title = $('#title').val();
+    var body = $('#body').val();
+
+    createIssue(repoName, repoOwner, title, body);
   });
 };
+
+$(document).ready(function(){
+  bindSubmitButton();
+});
