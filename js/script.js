a33e960df4311d6d72dedd984b36f37465e632b4
Logan McAnsh <l.m@loganmcansh.com>
2016-12-13 16:53:34 -0500

    Done.

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  let repoName, title, body;
+  submitForm();
+});
+
+Issue.prototype.renderIssue = function(elementSelector){
+  let link = $('<a>')
+    .attr('href', this.url)
+    .text(this.title);
+    elementSelector.append(link);
+}
+
+const interactor = new GithubInteractor("myAccessToken");
+
+function Issue(url, title, body){
+  this.url = url;
+  this.title = title;
+  this.body = body;
+}
+
+function GithubInteractor(token){
+  this.token = token;
+  this.url = "https://api.github.com/repos/";
+}
+
+function submitForm(){
+  $('form').on('submit', function(event){
+    let repoName = $('#repoName').val();
+    let repoOwner = $('#repoOwner').val();
+    let title = $('#title').val();
+    let body = $('#body').val();
+    createIssue(repoName, repoOwner, title, body);
+    event.preventDefault();
+  });
+}
+
+function handleResponse(response) {
+  let issue = new Issue(response.html_url, response.title, response.body)
+  issue.renderIssue($('#issue'));
+}
+
+function handleError(error) {
+  console.log(`Post error: ${error}`);
+}
+
+function createIssue(repoName, repoOwner, title, body){
+  let url = interactor.url + repoOwner + "/" + repoName + "/issues";
+  let data = {
+    title: title,
+    body: body
+  }
+  $.ajax({
+    type: "POST",
+    url: url,
+    beforeSend: function(response) {
+      response.setRequestHeader("Authorization", "token " + interactor.token);
+    },
+    data: JSON.stringify(data),
+  })
+  .done(handleResponse)
+  .fail(handleError);
+}
